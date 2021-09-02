ba8488d1c03bff1bbe113186a1d081fb759cff43
Lucas <lucasdamiangebhardt@gmail.com>
2021-09-02 13:56:10 -0300

    finalizado

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function validacion(evento){
     var expReglastName = /^([A-Z a-z]+[\s]*)+$/;
 
     var expRegID = /^\d+$/; 
-    var expRegBirthDate = /^\d+$/; 
+    var expRegBirthDate = /^([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])\2(\d{4})+$/; 
 
     var quedato = evento.target;
 
