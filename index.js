3337abe30858d41c68a4384c680d1cd7dc9a6109
Lucas <lucasdamiangebhardt@gmail.com>
2021-09-01 23:47:55 -0300

    sin estilo, funcionando

--- index.js
+++ index.js
@@ -1,29 +1,92 @@
 //Declaración de variables 
 var formkey = document.querySelector("#formkey");
-var name = document.querySelector("#name");
+var names = document.querySelector("#names");
 var lastName = document.querySelector("#lastName");
 var ID = document.querySelector("#ID")
 var birthDate = document.querySelector("#birthDate");
 var button = document.querySelector("#button");
+var respuesta = document.querySelector("#respuesta");
 
 
-//variables para expresiones regulares
+// variables para expresiones regulares
 
-
+// La clave tiene que tener 2 letras del nombre, 3 letras del apellido, el DNI completo y la fecha.
 
 function getKey(evento) 
 {
-//Generación de clave
-var clave = name.value.substring(0,3);
-clave += lastName.value.substring(0,2);
-clave += ID.value;
-clave += birthDate.value.substring(8);
-clave += birthDate.value.substring(3,5);
-clave += birthDate.value.substring(0,2);
+// Generación de clave
+var key = names.value.substring(0,2);
+key += lastName.value.substring(0,3);
+key += ID.value;
+key += birthDate.value.substring(8);
+key += birthDate.value.substring(3,5);
+key += birthDate.value.substring(0,2);
 
-respuesta.innerHTML = "Tu clave es:"+clave.toUpperCase();
+respuesta.innerHTML = "Tu clave es:"+key.toUpperCase();
 
     evento.preventDefault();
     alert ("Enviando Formulario");
 
 }
+
+function validacion(evento){
+    var expRegNames = /^([A-Z a-z]+[\s]*)+$/;
+
+    var expReglastName = /^([A-Z a-z]+[\s]*)+$/;
+
+    var expRegID = /^\d+$/; 
+    var expRegBirthDate = /^\d+$/; 
+
+    var quedato = evento.target;
+
+
+
+    if(quedato.id == 'names'){
+
+        if(!expRegNames.exec(quedato.value))
+           {
+            alert('Revise los datos ingresados, deben ser sólo letras');
+                // quedato.focus();
+         }
+    }
+
+    if(quedato.id == 'lastName'){
+
+        if(!expReglastName.exec(quedato.value))
+           {
+            alert('Revise los datos ingresados, deben ser sólo letras');
+                // quedato.focus();
+         }
+    }
+
+    if(quedato.id == 'ID'){
+
+        if(!expRegID.exec(quedato.value))
+           {
+            alert('Revise los datos ingresados, deben ser sólo numeros');
+                // quedato.focus();
+         }
+    }
+
+    if(quedato.id == 'birthDate'){
+
+        if(!expRegBirthDate.exec(quedato.value))
+           {
+            alert('Revise los datos ingresados, deben ser sólo numeros');
+                // quedato.focus();
+         }
+    }
+
+}
+
+
+function cargaDocu(){
+    names.addEventListener("blur",validacion);
+    lastName.addEventListener("blur",validacion);
+    ID.addEventListener("blur",validacion);
+    birthDate.addEventListener("blur",validacion);
+    formkey.addEventListener("submit",getKey);
+
+}
+
+window.addEventListener("load", cargaDocu);
